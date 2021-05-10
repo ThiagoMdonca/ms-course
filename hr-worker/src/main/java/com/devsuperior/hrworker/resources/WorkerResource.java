ac1972d747e65f53e73d23cbc725c91bbf56cd10
ThiagoMdonca <thiagolimamdonca@gmail.com>
2021-05-10 20:35:44 -0300

    Project hr-payrll created

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -13,21 +13,21 @@ import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
 
 @RestController
-@RequestMapping(value="/workers")
+@RequestMapping(value = "/workers")
 public class WorkerResource {
-	
+
 	@Autowired
 	private WorkerRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
-		List <Worker> list = repository.findAll();
+	public ResponseEntity<List<Worker>> findAll() {
+		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}
-	
-	@GetMapping(value="/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
-		Worker  obj = repository.findById(id).get();
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
 
